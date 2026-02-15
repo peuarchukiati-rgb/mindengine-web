ee6166d34c01efa5ea2a7028f6c11cea0df80879
peuarchukiati-rgb <p.euarchukiati@gmail.com>
2026-02-15 10:46:15 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
-xport default function Home() {
+export default function Home() {
   return (
-    <div style={{ background: 'black', color: 'lime', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'monospace' }}>
+    <div
+      style={{
+        background: "black",
+        color: "lime",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "monospace",
+      }}
+    >
       <h1>MIND ENGINE SYSTEM ONLINE</h1>
     </div>
-  )
+  );
 }
